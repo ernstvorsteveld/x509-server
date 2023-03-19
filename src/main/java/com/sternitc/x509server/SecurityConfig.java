7f4c7493abdd3f406d26e81e6ca722f8abfc84c7
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2023-03-19 16:06:46 +0100

    wip

--- src/main/java/com/sternitc/x509server/SecurityConfig.java
+++ src/main/java/com/sternitc/x509server/SecurityConfig.java
@@ -1,5 +1,13 @@
 package com.sternitc.x509server;
 
+import nl.altindag.ssl.SSLFactory;
+import nl.altindag.ssl.util.JettySslUtils;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +17,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -38,5 +48,33 @@ public class SecurityConfig {
                 throw new UsernameNotFoundException("User not found: " + cn);
             }
         };
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(SslContextFactory.Server sslContextFactory) {
+        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+        JettyServerCustomizer jettyServerCustomizer = server -> {
+            ServerConnector serverConnector = new ServerConnector(server, sslContextFactory);
+            serverConnector.setPort(8443);
+            server.setConnectors(new Connector[]{serverConnector});
+        };
+        factory.setServerCustomizers(Collections.singletonList(jettyServerCustomizer));
+        return factory;
+    }
+
+    @Bean
+    public SSLFactory sslFactory() {
+        return SSLFactory.builder()
+                .withSwappableIdentityMaterial()
+                .withIdentityMaterial("ssl/keystore.p12", "changeit".toCharArray())
+                .withSwappableTrustMaterial()
+                .withTrustMaterial("ssl/truststore.jks", "changeit".toCharArray())
+                .withNeedClientAuthentication()
+                .build();
+    }
+
+    @Bean
+    public SslContextFactory.Server sslContextFactory(SSLFactory sslFactory) {
+        return JettySslUtils.forServer(sslFactory);
     }
 }
